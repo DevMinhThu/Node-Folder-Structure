3e51f1af3cb886f007a7aab526e2094fca4fa02c
Minh Thư Vũ <devminhthu2023@gmail.com>
2021-04-26 23:07:57 +0700

    nodejs course

--- src/app/controllers/category.js
+++ src/app/controllers/category.js
@@ -1,9 +1,33 @@
 const CategoryModel = require("../models/category");
+const paginate = require("../../common/paginate");
 
 const index = async (req, res) => {
-  const categories = await CategoryModel.find();
+  // lấy trang hiện tại, nếu không có thì là trang 1
+  const page = parseInt(req.query.page) || 1;
 
-  res.render("admin/category", { categories: categories });
+  // giới hạn số phần tử trên 1 trang
+  const limit = 3;
+
+  // tính số phần tử sẽ bỏ qua
+  // ví dụ: mỗi trang có 3 phần tử, nếu muốn sang trang thứ 2 thì ta phải bỏ qua 3 phần tử
+  const skip = page * limit - limit;
+
+  // lấy ra tổng số phần tử
+  const total = await CategoryModel.find().countDocuments();
+
+  // tính tổng số trang.
+  // ví dụ: tính tổng xem có bao nhiêu trang con trong trang user
+  const totalPage = Math.ceil(total / limit);
+
+  const categories = await CategoryModel.find().skip(skip).limit(limit);
+
+  res.render("admin/category", {
+    categories: categories,
+    page: page,
+    totalPage: totalPage,
+    skip: skip,
+    pages: paginate(page, totalPage),
+  });
 };
 
 const create = (req, res) => {
--- src/app/controllers/product.js
+++ src/app/controllers/product.js
@@ -1,10 +1,37 @@
 const ProductModel = require("../models/product");
+const paginate = require("../../common/paginate");
 
 const index = async (req, res) => {
-  const products = await ProductModel.find().populate({ path: "cat_id" });
+  // lấy trang hiện tại, nếu không có thì là trang 1
+  const page = parseInt(req.query.page) || 1;
+
+  // giới hạn số phần tử trên 1 trang
+  const limit = 10;
+
+  // tính số phần tử sẽ bỏ qua
+  // ví dụ: mỗi trang có 3 phần tử, nếu muốn sang trang thứ 2 thì ta phải bỏ qua 3 phần tử
+  const skip = page * limit - limit;
+
+  // lấy ra tổng số phần tử
+  const total = await ProductModel.find().countDocuments();
+
+  // tính tổng số trang.
+  // ví dụ: tính tổng xem có bao nhiêu trang con trong trang user
+  const totalPage = Math.ceil(total / limit);
+
+  const products = await ProductModel.find()
+    .populate({ path: "cat_id" })
+    .skip(skip)
+    .limit(limit);
   // console.log(products);
 
-  res.render("admin/product", { products: products });
+  res.render("admin/product", {
+    products: products,
+    page: page,
+    totalPage: totalPage,
+    skip: skip,
+    pages: paginate(page, totalPage),
+  });
 };
 
 const create = (req, res) => {
